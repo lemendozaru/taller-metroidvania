6a2753e5e76ab961ff1f63de0b681a8869e35e78
lemendozar <mendozaruiz.luisenrique@gmail.com>
2026-06-24 19:04:51 -0600

    Script sobre máquina de estados agregado

---
+++ player/states/player_state.gd
@@ -0,0 +1,24 @@
+class_name PlayerState extends Node
+
+# Creamos variable player
+var player : Player
+
+# Creamos variable del siguiente estado
+var next_state : PlayerState 
+
+#region /// state references
+# Referencias a otros estados
+#endregion
+
+# Evento cuando el estado inicia
+func _init() -> void:
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
